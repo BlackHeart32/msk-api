25f51e2958a3136c9f0467aede398d5718aa08a8
BS32 <eguzmandls@gmail.com>
2024-01-05 01:44:08 -0500

    Initialcommit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Importing express module
+const express = require('express')
+//Instantiating express
+const app = express()
+
+//DEVELOPMENT ONLY
+//Listening to a port
+
+
+//PRODUCTION
+app.get('api/v1/orders', (req, res) =>{
+    const orders = 
+})
