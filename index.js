4f63328eb27016de9c18b7cae7727bbf5696cb2a
BS32 <eguzmandls@gmail.com>
2024-01-05 02:29:35 -0500

    Commented out mongocode

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ app.use((req, res, next) => {
 })
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
 //DEVELOPMENT ONLY
 //Listening to a port
 app.listen(5000, function () {
